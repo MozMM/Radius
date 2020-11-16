f7d34148dadfc6bf14a83b3adae4b60e1a424749
MozMM <67295821+MozMM@users.noreply.github.com>
2020-11-15 21:32:21 -0500

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -6,14 +6,7 @@ import * as firebase from 'firebase';
 import firestore from 'firebase/firestore'
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "radius-db5d1.firebaseapp.com",
-  databaseURL: "https://radius-db5d1.firebaseio.com",
-  projectId: "radius-db5d1",
-  storageBucket: "radius-db5d1.appspot.com",
-  messagingSenderId: "1088685100871",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: "G-58C436RVDN"
+
 };
 // Initialize Firebase
 firebase.initializeApp(config);
